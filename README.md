a9605299a5b12baca24b85efb7af8100685fcdae
Sami Saastamoinen <sami.saastamoinen@danfoss.com>
2019-09-12 10:49:31 +0300

    cleaned

--- README.md
+++ README.md
@@ -5,8 +5,7 @@ install python (>3.6)
 install libs:
 pip install setuptools
 pip install wave
-pip install numpy
 
 
 start:
-python -m wavepu
+python -m wavepulses.py
--- wavepulses.py
+++ wavepulses.py
@@ -1,6 +1,6 @@
 import wave
-import numpy as np
 
+#assuming 16bit mono file
 w = wave.open('360deg5min.wav', 'r')
 
 print (w.getparams())
@@ -10,20 +10,21 @@ print (w.getparams())
 #manually analyzed
 roughSignalAmplitude = 10800		#adjust this for the input signal
 
+#tune this to ensure all pulses are detected
 edgeTreshold = roughSignalAmplitude * 0.6
 
+#initialize
 sample = 0
 prevSample = 0
-prevSign = 0
-
 foundFallingEdges = 0
 foundRisingEdges = 0
+
+#number of samples in file
+nframes = w.getnframes()
 
 for i in range(1, nframes):
 	frame = w.readframes(1)
 	sample = int.from_bytes(frame, byteorder='little', signed=True)
-
-	sign = np.sign(sample)
 
 	changeDetected = False
 
@@ -35,9 +36,9 @@ for i in range(1, nframes):
 		changeDetected = True	
 		foundRisingEdges = foundRisingEdges + 1
 
+	#detect only changes that exceed the chosen treshold
 	if(changeDetected):
 		prevSample = sample
-		prevSign = sign
 
 	print("sample=%06d, value=%06d, falling=%06d, rising=%06d, full pulses=%06d" %(i, sample, foundFallingEdges, foundRisingEdges, (foundFallingEdges+foundRisingEdges)))
 
